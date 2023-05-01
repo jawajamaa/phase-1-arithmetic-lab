cd64d23f4619fc7594ac6869cb38c6235fe5b026
Timothy Ryon <tim@timryon.com>
2023-04-30 21:41:37 -0400

    finished Variablesgit add .

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 10;
+let num2 = 6;
+let num3 = num1;
+let num4 = num2;
+let multiply = (num1*num2)+2;
+let random = Math.ceil(Math.random()+1);
+let mod = mod=(num3%num4);
+let max = Math.max(5, 10, 20);    
 
-// Write your code here
+
